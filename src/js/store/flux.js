0302c16695538faccc86175cceb83f507f883f40
hector27gab <hectorgabriel270405@gmail.com>
2023-12-18 22:53:42 -0400

    webapp

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,45 +1,22 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+  return {
+    store: {
+      firstData: "Hello World",
+      apiURL: "https://rickandmortyapi.com/api/character",
+      characters: [],
+    },
+    actions: {
+      changeData: (newData) => {
+        setStore({ firstData: newData });
+      },
+      getApi: async () => {
+        const store = getStore();
+        const response = await fetch(store.apiURL);
+        const data = await response.json();
+        setStore({ characters: data.results });
+      },
+    },
+  };
 };
 
 export default getState;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,29 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import "../../styles/home.css";
+``;
+
+export const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+  const { store, actions } = useContext(Context);
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <p>{store.firstData}</p>
+        <input onChange={handleChange} />
+        <button
+          onClick={() => {
+            actions.changeData(inputValue);
+          }}
+        >
+          this button change the text
+        </button>
+      </div>
+    </>
+  );
+};
